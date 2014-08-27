a1952dcb61c067e585e3a57d5759163d47107902
inlost <boodleu@qq.com>
2014-08-27 18:01:01 +0800

    update logerror in CustomEvent

--- f2eApi/frontend/script/logError.js
+++ f2eApi/frontend/script/logError.js
@@ -1,27 +1,59 @@
 (function(undefined){
     var uid = 0,
         product = 'notSet'
-        logSender = new Image();
+        logSender = new Image(),
+        sendError = function(msg){
+            var params = [];
 
-    window.onerror = function(errorMessage, scriptURI, lineNumber,columnNumber,errorObj) {
-        var msg = {},
-            params = [];
+            msg.uid = uid;
+            msg.product = product;
 
-        msg.message = errorMessage;
-        msg.script = scriptURI.split('/').pop();
-        msg.position = 'L:' + lineNumber + ',C:' + (columnNumber || '-');
-        msg.uid = uid;
-        msg.product = product;
+            for(var key in msg){
+                params.push(key + '=' + encodeURIComponent(msg[key]));
+            }
+            params = params.join('&');
+            logSender.src = 'http://2eapi.hunteron.com/log/browser/error?' + params;
 
-        for(var key in msg){
-            params.push(key + '=' + encodeURIComponent(msg[key]));
-        }
-        params = params.join('&');
-        logSender.src = 'http://2eapi.hunteron.com/log/browser/error?' + params;
+            //TODO remove later
+            console.log(msg)
+        };
 
-        //TODO remove later
-        console.log(msg)
-    }
+    try{
+        (function(){
+            var useCustomEvent = false;
+            try{
+                var e = new CustomEvent('test');
+                useCustomEvent = true;
+            }catch(err){}
+
+            var handler = function(error){
+                var errorEvent = null;
+                if(useCustomEvent){
+                    errorEvent = new CustomEvent("logError", {detail: error});
+                }else{
+                  errorEvent = document.createEvent("Event");
+                  errorEvent.initEvent("logError", false, false);
+                  errorEvent.detail = error;
+                }
+                window.dispatchEvent(errorEvent);
+            }
+
+            if (window.addEventListener){
+                window.addEventListener("error", handler, false);
+            }else if(window.attachEvent){
+                window.attachEvent("error", handler);
+            }
+        })();
+    }catch(err){}
+    window.addEventListener('logError', function(e) {
+        var msg = {};
+
+        msg.message = e.detail.message;
+        msg.script = e.detail.filename.split('/').pop();
+        msg.position = 'L:' + e.detail.lineno + ',C:' + (e.detail.colno || '-');
+
+        sendError(msg);
+    }, false);
 
     window.logConfig = function(config){
         uid = config.uid;
